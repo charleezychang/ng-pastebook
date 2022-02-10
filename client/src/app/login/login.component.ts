d74ea4263d4fc50cfb79676c920d8cac55930ce2
jk <94419411+alegro-jk@users.noreply.github.com>
2022-02-10 20:09:01 +0800

    [feat] added stuff for fetch

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { FetchService } from './fetch.service';
+import { LoginModel } from './login.model';
 
 @Component({
   selector: 'app-login',
@@ -11,37 +14,58 @@ export class LoginComponent implements OnInit {
 
   isValidForm: boolean = false;
 
+  loginModelObj: LoginModel = new LoginModel();
+  
   userForm = new FormGroup({
-    email: new FormControl('', [
+    Email: new FormControl('', [
       Validators.required,
       Validators.pattern(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$/)
     ]),
-    password: new FormControl('', [
+    Password: new FormControl('', [
       Validators.required,
       Validators.minLength(8)
     ])
   });
 
-  constructor(private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, private formBuilder: FormBuilder, private service: FetchService ) { }
 
   ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      Email : [''],
+      Password : ['']
+    })
   }
 
+  // submitForm() {
+  //   this.isValidForm = false;
+  //    if (this.userForm.invalid) {
+  //       return;
+  //    }
+  //    this.isValidForm = true;
+  //    this.router.navigateByUrl("/");
+  // }
+
+  get Email() {
+    return this.userForm.get('Email');
+  }
+
+  get Password() {
+    return this.userForm.get('Password');
+  }
+
+  // postLoginDetails() {
   submitForm() {
-    this.isValidForm = false;
-     if (this.userForm.invalid) {
-        return;
-     }
-     this.isValidForm = true;
-     this.router.navigateByUrl("/");
-  }
+    this.loginModelObj.Email = this.userForm.value.Email;
+    this.loginModelObj.Password = this.userForm.value.Password;
 
-  get email() {
-    return this.userForm.get('email');
-  }
+    console.log(this.loginModelObj)
 
-  get password() {
-    return this.userForm.get('password');
+    this.service.postLogin(this.loginModelObj)
+    .subscribe(res=>{
+      alert("yazzz")
+      this.router.navigateByUrl("/");
+
+    })
   }
 
 }
